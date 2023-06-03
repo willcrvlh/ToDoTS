100df606525270a141b8ee046986d66221f561ed
Willian Carvalho <wcarvalho91610@gmail.com>
2023-06-03 17:04:15 -0300

    importei o footer e o header para a app

--- todots/src/App.tsx
+++ todots/src/App.tsx
@@ -1,11 +1,14 @@
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import S from './app.module.css'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <main className={S.main}>
       <p>CONTEUDO EM DESENVOLVIMENTO</p>
+      </main>
       <Footer/> 
     </div>
   );

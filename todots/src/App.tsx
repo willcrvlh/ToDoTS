0bf71ba96b3aa0cc9c87ccbd6f7af3c3a03c3333
Willian Carvalho <wcarvalho91610@gmail.com>
2023-06-03 11:14:40 -0300

    criei o header e o footer da pagina

--- todots/src/App.tsx
+++ todots/src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css';
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <p>CONTEUDO EM DESENVOLVIMENTO</p>
+      <Footer/>
     </div>
   );
 }
---
+++ todots/src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+export default function Footer () {
+  return (
+    <footer>
+        <span>
+            Projeto realizado por <a href="http://">Willian Carvalho</a>
+        </span>
+    </footer>
+  );
+}

fe5a8521764d2f7c6aeac4874722a814e1bf39c7
Willian Carvalho <wcarvalho91610@gmail.com>
2023-06-29 13:33:48 -0300

    importei as listas para a app

--- todots/src/App.tsx
+++ todots/src/App.tsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import TaskForm from "./Components/TaskForm/TaskForm";
-import TaskList from "./Components/TaskList/TaskList"
-import {ITask} from "./Interfaces/Task"
-import S from './app.module.css'
+import TaskList from "./Components/TaskList/TaskList";
+import { ITask } from "./Interfaces/Task";
+import S from "./app.module.css";
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <main className={S.main}>
-      <TaskForm btnText="criar tarefa"/>
-      <TaskList/>
+        <TaskForm
+          btnText="criar tarefa"
+          taskList={taskList}
+          setTaskList={setTaskList}
+        />
+        <TaskList />
       </main>
-      <Footer/> 
+      <Footer />
     </div>
   );
 }
